a1bce2406461886fd3dfb3c74592afa5530c081a
Vedant Yaduvanshi-) <87130565+vedantyaduvanshi@users.noreply.github.com>
2024-01-15 16:15:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import Editor from './components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Editor.jsx
@@ -0,0 +1,39 @@
+// src/components/Editor.jsx
+import React from 'react';
+import './Editor.css';
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: '',
+    };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="input">
+          <h3>Input</h3>
+          <textarea
+            className="input-text"
+            onChange={this.handleChange}
+            value={this.state.value}
+          />
+        </div>
+        <div className="output">
+          <h3>Pro Note</h3>
+          <div className="output-text">{this.state.value}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Editor;
